c8ea34143dd4bc7a4d3b170193189a249a01751e
Hooni-i <rlagns0904@gmail.com>
2025-04-12 01:51:01 +0900

    refactor: 잘못된 그룹명 수정

--- src/main/java/com/fourirbnb/config/ConfigApplication.java
+++ src/main/java/com/fourirbnb/config/ConfigApplication.java
@@ -1,4 +1,4 @@
-package com.fouribnb.config;
+package com.fourirbnb.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigApplication.class, args);
+  }
 
 }
--- src/test/java/com/fourirbnb/config/ConfigApplicationTests.java
+++ src/test/java/com/fourirbnb/config/ConfigApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fouribnb.config;
+package com.fourirbnb.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConfigApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
